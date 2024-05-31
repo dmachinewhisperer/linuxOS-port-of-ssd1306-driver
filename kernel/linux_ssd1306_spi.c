a63a68959d1a72143d0f6110d5dfa02c446334d8
Asogwa Emmanuel <asogwaemmanuel36@gmail.com>
2024-05-31 13:13:09 +0100

    support all spi func

--- kernel/linux_ssd1306_spi.c
+++ kernel/linux_ssd1306_spi.c
@@ -8,3 +8,12 @@ s32 spi_init(SSD1306_t *dev, int width, int height){
 s32 spi_display_image(SSD1306_t * dev, int page, int seg, uint8_t * images, int width){
     return 0;
 }
+
+
+s32 spi_contrast(SSD1306_t * dev, int contrast){
+    return 0;
+}
+
+s32 spi_hardware_scroll(SSD1306_t * dev, ssd1306_scroll_type_t scroll){
+    return 0;
+}
